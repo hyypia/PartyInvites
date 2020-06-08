42ed71b1fe51a48f571976c2b5eeb56b70c68a1a
hyypia <igortereschuk92@gmail.com>
2020-06-08 19:58:15 +0200

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponce guestResponce)
+        {
+            Repository.AddResponce(guestResponce);
+            return View("Thanks", guestResponce);
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponce> responces = new List<GuestResponce>();
+        public static IEnumerable<GuestResponce> Responces { get { return responces; } }
+
+        public static void AddResponce(GuestResponce responce)
+        {
+            responces.Add(responce);
+        }
+    }
+}
